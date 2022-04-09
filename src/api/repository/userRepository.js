98e37c7885ec3cfe7b4246503bc8ef9429c4a2ad
matheo <math.sou@hotmail.com>
2022-04-09 17:21:26 -0300

    repository com as conexões necessárias para o CRUD de usuários

--- src/api/repository/userRepository.js
+++ src/api/repository/userRepository.js
@@ -3,5 +3,28 @@ const userModel = require("../model/userModel");
 module.exports = {
   async findUsers() {
     return await userModel.findAll();
+  },
+  async findOneUser(id) {
+    return await userModel.findOne({
+      where: {id}
+    });
+  },
+  async findEmailExists(email) {
+    return await userModel.findOne({
+      where: {email}
+    });
+  },
+  async registerUser(payload) {
+    return await userModel.build(payload).save();
+  },
+  async updateUser(id, payload) {
+    return await userModel.update(payload, {
+      where: {id}
+    });
+  },
+  async deleteUser(id) {
+    return await userModel.destroy({
+      where: { id },
+    });
   }
 };
