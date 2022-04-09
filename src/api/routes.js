47de1d18dd31c69b3f7562040ca42f33fb0d495e
matheo <math.sou@hotmail.com>
2022-04-09 17:22:50 -0300

    rotas do CRUD de usuários

--- src/api/routes.js
+++ src/api/routes.js
@@ -3,7 +3,11 @@ const routes = express.Router();
 
 const userController = require("./controller/userController");
 
-routes.get("/", userController.findAll);
+routes.get("/users", userController.findAll);
+routes.get("/users/:id", userController.findOne);
+routes.post("/users", userController.create);
+routes.delete("/users/:id", userController.delete);
+routes.put("/users/:id", userController.update);
 
 
 module.exports = routes;
