2b10f96041ff53a2d79ec3584128cce247dba451
matheo <math.sou@hotmail.com>
2022-04-09 17:22:00 -0300

    regras  de negócio para os usuários

--- src/api/controller/userController.js
+++ src/api/controller/userController.js
@@ -5,6 +5,35 @@ module.exports = {
     const { query } = req;
     const users = await userService.findUsers();
     res.status(200).json(users);
+  },
+  async findOne(req, res) {
+    const { query, params } = req;
+    const users = await userService.findOneUser(params.id);
+    res.status(200).json(users);
+  },
+  async create(req, res) {
+    const { body } = req;
+    const createdUser = await userService.registerNewUser(body);
+    if (createdUser.error) {
+      res.status(403).json(createdUser);
+    } else {
+      res.status(200).json({ id: createdUser, message: "Usuário cadastrado com sucesso" });
+    }
+  },
+  async update(req, res) {
+    const { params, body } = req;
+    const rowsAffected = await userService.updateUser(params.id, body);
+    if (rowsAffected)
+      res.status(200).json({ message: "Usuário atualizado com sucesso" });
+    else
+      res.status(400).json({ error: true, message: "Usuário não encontrado" })
+  },
+  async delete(req, res) {
+    const { params } = req;
+    const rowsAffected = await userService.deleteUser(params.id);
+    if (rowsAffected)
+      res.status(200).json({ message: "Usuário excluído com sucesso" });
+    else
+      res.status(400).json({ error: true, message: "Usuário não encontrado" })
   }
-  
 };
--- src/api/service/userService.js
+++ src/api/service/userService.js
@@ -1,9 +1,51 @@
+const md5 = require("md5");
 const userRepository = require("../repository/userRepository");
 
 
 module.exports = {
   async findUsers() {
-    const rows  = await userRepository.findUsers();
+    const rows = await userRepository.findUsers();
     return { data: rows };
+  },
+  async findOneUser(id) {
+    const rows = await userRepository.findOneUser(id);
+    return { data: rows };
+  },
+  async registerNewUser(fields) {
+    if (await userRepository.findEmailExists(fields.email))
+      return { error: true, message: "E-mail já cadastrado" };
+    const createdUser = await userRepository.registerUser({
+      name: fields.name,
+      email: fields.email,
+      password: md5(fields.password),
+      phone: fields.phone
+    });
+    return createdUser.id;
+  },
+  async updateUser(id, fields) {
+    let payload = {
+      name: fields.name,
+      email: fields.email,
+      phone: fields.phone
+    };
+    if (fields.password)
+      payload.password = md5(fields.password);
+    const rowsAffected = await userRepository.updateUser(id, payload);
+    return rowsAffected[0];
+  },
+  async updateUser(id, fields) {
+    let payload = {
+      name: fields.name,
+      email: fields.email,
+      phone: fields.phone
+    };
+    if (fields.password)
+      payload.password = md5(fields.password);
+    const rowsAffected = await userRepository.updateUser(id, payload);
+    return rowsAffected[0];
+  },
+  async deleteUser(id) {
+    const rowsAffected = await userRepository.deleteUser(id);
+    return rowsAffected;
   }
 };
